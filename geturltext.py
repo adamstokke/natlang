a7a55729787958a2a6c8c1997aa1243b465cc547
Adam Stokke <adamstokke@gmail.com>
2014-10-25 23:53:10 -0700

    Add geturltext

--- geturltext.py
+++ geturltext.py
@@ -1,13 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import urllib
 from bs4 import BeautifulSoup
 import nltk
 
 def geturltext(url):
+    """Given a valid url, print information about the textual content
+       contained within the data found at that url. Not guaranteed,
+       at present, to resturn 'optimal' results."""
     response = urllib.urlopen(url)
     raw = response.read().decode('utf-8')
 
     doc = BeautifulSoup(raw)
-    print 'document title: ', doc.title.string
+    print 'document title: ', doc.title.string, '\n'
     #print 'document head: ', doc.head
     #print 'document text: ', doc.get_text()
 
