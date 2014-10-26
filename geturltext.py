ab2a44cbc4d59a907a76ef29b0a5bf1f110c6bff
Adam Stokke <adamstokke@gmail.com>
2014-10-26 02:06:54 -0700

    Added placeholder for additional filtering bits

--- geturltext.py
+++ geturltext.py
@@ -2,13 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import urllib
+import re
 from bs4 import BeautifulSoup
 import nltk
 
-def geturltext(url):
+def geturltext(url, normalize=True):
     """Given a valid url, print information about the textual content
        contained within the data found at that url. Not guaranteed,
-       at present, to resturn 'optimal' results."""
+       at present, to resturn 'optimal' results.
+
+       Normalization is turned on by default; this lowercases all
+       words in a text before considering its vocabulary."""
     response = urllib.urlopen(url)
     raw = response.read().decode('utf-8')
 
@@ -26,21 +30,23 @@ def geturltext(url):
     # here, prune the words by getting rid of code bits that show
     # up as words (see nltk book 3.4, etc.)
     somewords = [w for w in text if "=" not in w]
+    # this may omit decimal numbers that are part of the text
     somewords = [w for w in somewords if "." not in w]
     somewords = [w for w in somewords if "_" not in w]
     somewords = [w for w in somewords if "+" not in w]
     somewords = [w for w in somewords if "/" not in w]
+    # words with two hyphens in them are usually code
+    #twohyphenwords = [w for w in somewords if re.search('^[a-z]+-[a-z]+-[a-z]$', w)]
+    #somewords = [w for w in somewords if w not in twohyphenwords]
     
-    words = [w.lower() for w in somewords]
+    if normalize:
+        somewords = [w.lower() for w in somewords]
+        
+    words = somewords
     vocab = sorted(set(words))
 
     print "# of unique words: ", len(vocab), '\n'
 
     #print vocab
-
-    
-
-
-
-
+    return vocab
 
